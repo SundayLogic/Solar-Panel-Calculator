10c06a5c1c768e33af641e002109952c5d0a5651
Pablo <contactoemail.pablo@gmail.com>
2023-06-22 22:59:45 +0200

    Adding trigonometry: Fixing Circle Duplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import Canvas from "./components/molecules/Canvas";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App: React.FC = () => {
   const [angle, setAngle] = useState(0);
 
   return (
-    <div>
-      <input
-        type="range"
-        min="0"
-        max="360"
-        value={angle}
-        onChange={(e) => setAngle(Number(e.target.value))}
-      />
-      <Canvas angle={angle} />
+    <div className="flex justify-center items-center h-[100vh] bg-black">
+      <div className="flex flex-col justify-center items-center">
+        <Canvas angle={angle} canvasHeight={500} circleWidth={120} canvasWidth={400}/>
+        <input
+          type="range"
+          min="0"
+          max="360"
+          value={angle}
+          onChange={(e) => setAngle(Number(e.target.value))}
+        />
+      </div>
     </div>
   );
 };
--- src/components/molecules/Canvas.tsx
+++ src/components/molecules/Canvas.tsx
@@ -1,62 +1,69 @@
 import React, { useEffect, useRef } from 'react';
 import p5 from 'p5';
 
-const canvasWidth = 800;
-const canvasHeight = 800;
-
 interface P5WithProps extends p5 {
-  props: { angle: number };
+  props: { angle: number; canvasWidth: number; canvasHeight: number; circleWidth: number };
 }
 
 interface CanvasProps {
   angle: number;
+  canvasWidth: number;
+  canvasHeight: number;
+  circleWidth: number;
 }
 
-const Canvas: React.FC<CanvasProps> = ({ angle }) => {
+const Canvas: React.FC<CanvasProps> = ({ angle, canvasWidth, canvasHeight, circleWidth }) => {
   const canvasRef = useRef<HTMLDivElement>(null);
-  const p5Ref = useRef<P5WithProps>();
-
-  const sketch = (p: P5WithProps) => {
-    p.setup = () => {
-      p.createCanvas(canvasWidth, canvasHeight);
-      p.angleMode(p.DEGREES);
-    };
-
-    p.draw = () => {
-      p.background(0);
-      p.translate(p.width / 2, p.height / 2);
-
-      p.stroke(255);
-      p.noFill();
-      p.ellipse(0, 0, canvasHeight * 0.6);
-
-      p.stroke(255);
-      const x = Math.cos(p.props.angle) * canvasHeight * 0.3;
-      const y = -Math.sin(p.props.angle) * canvasHeight * 0.3;
-      p.line(0, 0, x, y);
-
-      p.stroke('blue');
-      p.line(0, 0, x, 0);
-      p.stroke('red');
-      p.line(0, 0, 0, y);
-    };
-  };
+  const p5Ref = useRef<P5WithProps | null>(null);
 
   useEffect(() => {
-    p5Ref.current = new p5(sketch as any, canvasRef.current as HTMLElement) as P5WithProps; 
-    return () => {
-      p5Ref.current?.remove();
-    };
-  }, []);
+    const sketch = (p: P5WithProps) => {
+      p.setup = () => {
+        p.createCanvas(canvasWidth, canvasHeight);
+        p.angleMode(p.DEGREES);
+      };
 
-  useEffect(() => {
+      p.draw = () => {
+        p.background(0);
+        p.translate(p.width / 2, p.height / 2);
+
+        const radius = p.height * 0.3;
+        const x = p.cos(angle) * radius;
+        const y = -p.sin(angle) * radius;
+
+        p.stroke(255);
+        p.noFill();
+        p.ellipse(0, 0, circleWidth);
+
+        p.stroke(255);
+        p.line(0, 0, x, y);
+
+        p.stroke('yellow');
+        p.line(0, 0, x, 0);
+
+        p.stroke('red');
+        p.line(x, 0, x, y);
+      };
+    };
+
+    if (canvasRef.current && !p5Ref.current) {
+      p5Ref.current = new p5(sketch, canvasRef.current) as P5WithProps;
+    }
+
     if (p5Ref.current) {
-      p5Ref.current.props = { angle };
+      p5Ref.current.props = { angle, canvasWidth, canvasHeight, circleWidth };
       p5Ref.current.redraw();
     }
-  }, [angle]);
 
-  return <div ref={canvasRef}></div>;
+    return () => {
+      if (p5Ref.current) {
+        p5Ref.current.remove();
+        p5Ref.current = null;
+      }
+    };
+  }, [angle, canvasWidth, canvasHeight, circleWidth]);
+
+  return <div ref={canvasRef} />;
 };
 
 export default Canvas;
